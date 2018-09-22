d8612851d3d206e9b6728f87de63833d2fddbade
varun-experion <varun.chandran@experionglobal.com>
2018-09-22 12:23:18 +0530

    adapter commit

---
+++ app/src/main/java/com/example/varun/learningkotlinmvvm/Adapter/CustomAdapter.kt
@@ -0,0 +1,45 @@
+package com.example.varun.learningkotlinmvvm.Adapter
+
+import android.content.Context
+import android.databinding.DataBindingUtil
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.example.varun.learningkotlinmvvm.R
+import com.example.varun.learningkotlinmvvm.ViewModel.UserViewModel
+import com.example.varun.learningkotlinmvvm.databinding.UserBinding
+
+class CustomAdapter(private val context: Context, private val arrayList: ArrayList<UserViewModel>) : RecyclerView.Adapter<CustomAdapter.CustomView>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomView {
+
+        val layoutInflater = LayoutInflater.from(parent.context)
+
+        val userBinding: UserBinding = DataBindingUtil.inflate(layoutInflater, R.layout.fragment_write, parent, false)
+        return CustomView(userBinding)
+    }
+
+    override fun getItemCount(): Int {
+
+        return arrayList.size
+    }
+
+    override fun onBindViewHolder(holder: CustomView, position: Int) {
+
+        val userViewModel = arrayList[position]
+        holder.bind(userViewModel)
+    }
+
+
+    class CustomView(val userBinding: UserBinding) : RecyclerView.ViewHolder(userBinding.root) {
+
+        fun bind(userViewModel: UserViewModel) {
+
+
+            this.userBinding.usermodel = userViewModel
+            userBinding.executePendingBindings()
+        }
+
+
+    }
+
+}
--- app/src/main/java/com/example/varun/learningkotlinmvvm/Model/User.kt
+++ app/src/main/java/com/example/varun/learningkotlinmvvm/Model/User.kt
@@ -3,22 +3,16 @@ package com.example.varun.learningkotlinmvvm.Model
 import java.util.*
 
 
-class User: Observable() {
-var name:String=""
-    set(value) {
-        field=value
-        setChangedAndNotify("name")
-    }
+class User {
 
-    var email: String = ""
-        set(value) {
-            field = value
-            setChangedAndNotify("email")
-        }
 
-    private fun setChangedAndNotify(field: Any)
-    {
-        setChanged()
-        notifyObservers(field)
+    var id = ""
+    var name = ""
+    var email = ""
+
+    constructor(id: String, name: String, email: String) {
+        this.id = id
+        this.name = name
+        this.email = email
     }
 }
